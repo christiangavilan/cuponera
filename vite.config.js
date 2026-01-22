b9e252352d6af7da6a7382f4d6edc567ed7d4c76
christiangavilan <christiangavilan@gmail.com>
2026-01-22 17:26:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://markobarraza.github.io/cuponera/'
+  base: 'https://christiangavilan.github.io/cuponera/'
 })
